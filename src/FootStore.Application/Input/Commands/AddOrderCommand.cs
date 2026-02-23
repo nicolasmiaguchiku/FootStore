9cf093a470412a311bc64fdcaaa9265b2757bbfa
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-02-22 23:55:49 -0300

    feat: endpoint create  to add order

---
+++ src/FootStore.Application/Input/Commands/AddOrderCommand.cs
@@ -0,0 +1,8 @@
+﻿using FootStore.Application.Requests;
+using FootStore.Application.Responses;
+using FootStore.Domain.Models;
+using LiteBus.Commands.Abstractions;
+
+namespace FootStore.Application.Input.Commands;
+
+public record AddOrderCommand(AddOrderRequest Request) : ICommand<Result<OrderResponse>>;
