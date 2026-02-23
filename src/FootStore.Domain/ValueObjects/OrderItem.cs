5433546423c4e66e3246f657c7c6453d2e793c0c
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-02-22 23:19:09 -0300

    feat: created entitie order

---
+++ src/FootStore.Domain/ValueObjects/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿using FootStore.Domain.Entities;
+
+namespace FootStore.Domain.ValueObjects
+{
+    public class OrderItem
+    {
+        public ProductEntity Product { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
